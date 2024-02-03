9b4a5598440415aa4c31b5bfefcf265781ac6c23
AtticusFinchIPHEclipseWindow <tranvan.duc.012@gmail.com>
2024-02-03 14:58:50 +0700

    Config Security

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -0,0 +1,17 @@
+package com.udacity.jwdnd.course1.cloudstorage.mappers;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Select("SELECT * FROM USERS WHERE userName = #{userName}")
+    User getUser(String userName);
+
+    @Insert("INSERT INTO USERS (userName, salt, password, firstName, lastName) VALUE (#{userName}, #{salt}, #{password}, #{firstName}, #{lastName}")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(User user);
+}
